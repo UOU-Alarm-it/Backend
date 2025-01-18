e292610e01b468e60ec4be1c8d0b103939647e52
Gihyeon <ryuoo0123@gmail.com>
2025-01-18 17:09:57 +0900

    init commit

---
+++ src/main/java/uou/alarmit/AlarmItApplication.java
@@ -0,0 +1,13 @@
+package uou.alarmit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlarmItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uou/alarmit/AlarmItApplicationTests.java
@@ -0,0 +1,13 @@
+package uou.alarmit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
