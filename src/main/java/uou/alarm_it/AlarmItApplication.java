d4cb18020f7c6bef0f2e4b99fe7d1cf1cbeb33f4
Gihyeon <ryuoo0123@gmail.com>
2025-01-18 19:05:21 +0900

    scheduling dynamic update fin

--- src/main/java/uou/alarm_it/AlarmItApplication.java
+++ src/main/java/uou/alarm_it/AlarmItApplication.java
@@ -2,8 +2,10 @@ package uou.alarm_it;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AlarmItApplication {
 
     public static void main(String[] args) {
