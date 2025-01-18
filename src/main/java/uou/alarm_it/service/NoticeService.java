039b4fbb95679e793bec8d456eb64e5a9a201a13
Gihyeon <ryuoo0123@gmail.com>
2025-01-18 20:00:50 +0900

    view notice

--- src/main/java/uou/alarm_it/service/NoticeService.java
+++ src/main/java/uou/alarm_it/service/NoticeService.java
@@ -1,5 +1,6 @@
 package uou.alarm_it.service;
 
+import uou.alarm_it.domain.Enum.Category;
 import uou.alarm_it.domain.Notice;
 
 import java.util.List;
@@ -9,4 +10,6 @@ public interface NoticeService {
     List<Notice> webCrawling(Integer page);
 
     void scheduledUpdate();
+
+    List<Notice> findByCategory(Integer categoryInt);
 }
